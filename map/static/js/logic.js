a4c08e4a6ee03b23a60dbc2b105a2ab16bfa350e
lukecrabtree <luke@Administrators-MacBook-Pro-2.local>
2022-07-16 03:15:23 -0400

    3 maps

--- map/static/js/logic.js
+++ map/static/js/logic.js
@@ -51,6 +51,21 @@ d3.json(url).then(function(response) {
 console.log(response)
 typeMarkers = [];
 zoneMarkers = [];
+larcenyVMarkers = [];
+larcenyNVMarkers = [];
+aggAssaultMarkers = [];
+robberyMarkers = [];
+burglaryMarkers = [];
+autoTheftMarkers = [];
+homicideMarkers = [];
+mondayMarkers = [];
+tuesdayMarkers = [];
+wednesdayMarkers = [];
+thursdayMarkers = [];
+fridayMarkers = [];
+saturdayMarkers = [];
+sundayMarkers = [];
+
     for (var i = 0; i < 20315; i++) {
         
         console.log(response.zone[i])  
@@ -64,7 +79,6 @@ zoneMarkers = [];
                 //radius: 100
             }).bindPopup(`<h3>Offense ID: ${response.offense_id[i]}</h3><hr><p>${response.crime_type[i]}</p><p>ZONE ${response.zone[i]}</p><p>${response.neighborhood[i]}</p><p>${response.location[i]}</p><p>${response.occur_time[i]} ${response.occur_day[i]} ${response.occur_date[i]}</p>`)
         )   
- 
         zoneMarkers.push(
             L.circle([response.lat[i], response.long[i]], 
                 {
@@ -76,6 +90,179 @@ zoneMarkers = [];
                 }).bindPopup(`<h3>Offense ID: ${response.offense_id[i]}</h3><hr><p>${response.crime_type[i]}</p><p>ZONE ${response.zone[i]}</p><p>${response.neighborhood[i]}</p><p>${response.location[i]}</p><p>${response.occur_time[i]} ${response.occur_day[i]} ${response.occur_date[i]}</p>`)
             ) 
 
+        if(response.crime_type[i] == "LARCENY-FROM VEHICLE"){
+            larcenyVMarkers.push(
+                L.circle([response.lat[i], response.long[i]], 
+                    {
+                        //stroke: false,
+                        fillOpacity: 0.75,
+                        color: chooseColorZone(response.crime_type[i]),
+                        fillColor: chooseColorZone(response.crime_type[i]),
+                        //radius: 100
+                    }).bindPopup(`<h3>Offense ID: ${response.offense_id[i]}</h3><hr><p>${response.crime_type[i]}</p><p>ZONE ${response.zone[i]}</p><p>${response.neighborhood[i]}</p><p>${response.location[i]}</p><p>${response.occur_time[i]} ${response.occur_day[i]} ${response.occur_date[i]}</p>`)
+                )
+        }
+        else if(response.crime_type[i] == "LARCENY-NON VEHICLE"){
+            larcenyNVMarkers.push(
+                L.circle([response.lat[i], response.long[i]], 
+                    {
+                        //stroke: false,
+                        fillOpacity: 0.75,
+                        color: chooseColorZone(response.crime_type[i]),
+                        fillColor: chooseColorZone(response.crime_type[i]),
+                        //radius: 100
+                    }).bindPopup(`<h3>Offense ID: ${response.offense_id[i]}</h3><hr><p>${response.crime_type[i]}</p><p>ZONE ${response.zone[i]}</p><p>${response.neighborhood[i]}</p><p>${response.location[i]}</p><p>${response.occur_time[i]} ${response.occur_day[i]} ${response.occur_date[i]}</p>`)
+                )
+        }
+        else if(response.crime_type[i] == "AGG ASSAULT"){
+            aggAssaultMarkers.push(
+                L.circle([response.lat[i], response.long[i]], 
+                    {
+                        //stroke: false,
+                        fillOpacity: 0.75,
+                        color: chooseColorZone(response.crime_type[i]),
+                        fillColor: chooseColorZone(response.crime_type[i]),
+                        //radius: 100
+                    }).bindPopup(`<h3>Offense ID: ${response.offense_id[i]}</h3><hr><p>${response.crime_type[i]}</p><p>ZONE ${response.zone[i]}</p><p>${response.neighborhood[i]}</p><p>${response.location[i]}</p><p>${response.occur_time[i]} ${response.occur_day[i]} ${response.occur_date[i]}</p>`)
+                )
+        }
+        else if(response.crime_type[i] == "ROBBERY"){
+            robberyMarkers.push(
+                L.circle([response.lat[i], response.long[i]], 
+                    {
+                        //stroke: false,
+                        fillOpacity: 0.75,
+                        color: chooseColorZone(response.crime_type[i]),
+                        fillColor: chooseColorZone(response.crime_type[i]),
+                        //radius: 100
+                    }).bindPopup(`<h3>Offense ID: ${response.offense_id[i]}</h3><hr><p>${response.crime_type[i]}</p><p>ZONE ${response.zone[i]}</p><p>${response.neighborhood[i]}</p><p>${response.location[i]}</p><p>${response.occur_time[i]} ${response.occur_day[i]} ${response.occur_date[i]}</p>`)
+                )
+        }
+        else if(response.crime_type[i] == "BURGLARY"){
+            burglaryMarkers.push(
+                L.circle([response.lat[i], response.long[i]], 
+                    {
+                        //stroke: false,
+                        fillOpacity: 0.75,
+                        color: chooseColorZone(response.crime_type[i]),
+                        fillColor: chooseColorZone(response.crime_type[i]),
+                        //radius: 100
+                    }).bindPopup(`<h3>Offense ID: ${response.offense_id[i]}</h3><hr><p>${response.crime_type[i]}</p><p>ZONE ${response.zone[i]}</p><p>${response.neighborhood[i]}</p><p>${response.location[i]}</p><p>${response.occur_time[i]} ${response.occur_day[i]} ${response.occur_date[i]}</p>`)
+                )
+        }
+        else if(response.crime_type[i] == "AUTO THEFT"){
+            autoTheftMarkers.push(
+                L.circle([response.lat[i], response.long[i]], 
+                    {
+                        //stroke: false,
+                        fillOpacity: 0.75,
+                        color: chooseColorZone(response.crime_type[i]),
+                        fillColor: chooseColorZone(response.crime_type[i]),
+                        //radius: 100
+                    }).bindPopup(`<h3>Offense ID: ${response.offense_id[i]}</h3><hr><p>${response.crime_type[i]}</p><p>ZONE ${response.zone[i]}</p><p>${response.neighborhood[i]}</p><p>${response.location[i]}</p><p>${response.occur_time[i]} ${response.occur_day[i]} ${response.occur_date[i]}</p>`)
+                )
+        }
+        else if(response.crime_type[i] == "HOMICIDE"){
+            homicideMarkers.push(
+                L.circle([response.lat[i], response.long[i]], 
+                    {
+                        //stroke: false,
+                        fillOpacity: 0.75,
+                        color: chooseColorZone(response.crime_type[i]),
+                        fillColor: chooseColorZone(response.crime_type[i]),
+                        //radius: 100
+                    }).bindPopup(`<h3>Offense ID: ${response.offense_id[i]}</h3><hr><p>${response.crime_type[i]}</p><p>ZONE ${response.zone[i]}</p><p>${response.neighborhood[i]}</p><p>${response.location[i]}</p><p>${response.occur_time[i]} ${response.occur_day[i]} ${response.occur_date[i]}</p>`)
+                )
+        }
+
+
+        if(response.occur_day[i] == "Monday"){
+            mondayMarkers.push(
+                L.circle([response.lat[i], response.long[i]], 
+                    {
+                        //stroke: false,
+                        fillOpacity: 0.75,
+                        color: chooseColorZone(response.crime_type[i]),
+                        fillColor: chooseColorZone(response.crime_type[i]),
+                        //radius: 100
+                    }).bindPopup(`<h3>Offense ID: ${response.offense_id[i]}</h3><hr><p>${response.crime_type[i]}</p><p>ZONE ${response.zone[i]}</p><p>${response.neighborhood[i]}</p><p>${response.location[i]}</p><p>${response.occur_time[i]} ${response.occur_day[i]} ${response.occur_date[i]}</p>`)
+                )
+        }
+        else if(response.occur_day[i] == "Tuesday"){
+            tuesdayMarkers.push(
+                L.circle([response.lat[i], response.long[i]], 
+                    {
+                        //stroke: false,
+                        fillOpacity: 0.75,
+                        color: chooseColorZone(response.crime_type[i]),
+                        fillColor: chooseColorZone(response.crime_type[i]),
+                        //radius: 100
+                    }).bindPopup(`<h3>Offense ID: ${response.offense_id[i]}</h3><hr><p>${response.crime_type[i]}</p><p>ZONE ${response.zone[i]}</p><p>${response.neighborhood[i]}</p><p>${response.location[i]}</p><p>${response.occur_time[i]} ${response.occur_day[i]} ${response.occur_date[i]}</p>`)
+                )
+        }
+        else if(response.occur_day[i] == "Wednesday"){
+            wednesdayMarkers.push(
+                L.circle([response.lat[i], response.long[i]], 
+                    {
+                        //stroke: false,
+                        fillOpacity: 0.75,
+                        color: chooseColorZone(response.crime_type[i]),
+                        fillColor: chooseColorZone(response.crime_type[i]),
+                        //radius: 100
+                    }).bindPopup(`<h3>Offense ID: ${response.offense_id[i]}</h3><hr><p>${response.crime_type[i]}</p><p>ZONE ${response.zone[i]}</p><p>${response.neighborhood[i]}</p><p>${response.location[i]}</p><p>${response.occur_time[i]} ${response.occur_day[i]} ${response.occur_date[i]}</p>`)
+                )
+        }
+        else if(response.occur_day[i] == "Thursday"){
+            thursdayMarkers.push(
+                L.circle([response.lat[i], response.long[i]], 
+                    {
+                        //stroke: false,
+                        fillOpacity: 0.75,
+                        color: chooseColorZone(response.crime_type[i]),
+                        fillColor: chooseColorZone(response.crime_type[i]),
+                        //radius: 100
+                    }).bindPopup(`<h3>Offense ID: ${response.offense_id[i]}</h3><hr><p>${response.crime_type[i]}</p><p>ZONE ${response.zone[i]}</p><p>${response.neighborhood[i]}</p><p>${response.location[i]}</p><p>${response.occur_time[i]} ${response.occur_day[i]} ${response.occur_date[i]}</p>`)
+                )
+        }
+        else if(response.occur_day[i] == "Friday"){
+            fridayMarkers.push(
+                L.circle([response.lat[i], response.long[i]], 
+                    {
+                        //stroke: false,
+                        fillOpacity: 0.75,
+                        color: chooseColorZone(response.crime_type[i]),
+                        fillColor: chooseColorZone(response.crime_type[i]),
+                        //radius: 100
+                    }).bindPopup(`<h3>Offense ID: ${response.offense_id[i]}</h3><hr><p>${response.crime_type[i]}</p><p>ZONE ${response.zone[i]}</p><p>${response.neighborhood[i]}</p><p>${response.location[i]}</p><p>${response.occur_time[i]} ${response.occur_day[i]} ${response.occur_date[i]}</p>`)
+                )
+        }
+        else if(response.occur_day[i] == "Saturday"){
+            saturdayMarkers.push(
+                L.circle([response.lat[i], response.long[i]], 
+                    {
+                        //stroke: false,
+                        fillOpacity: 0.75,
+                        color: chooseColorZone(response.crime_type[i]),
+                        fillColor: chooseColorZone(response.crime_type[i]),
+                        //radius: 100
+                    }).bindPopup(`<h3>Offense ID: ${response.offense_id[i]}</h3><hr><p>${response.crime_type[i]}</p><p>ZONE ${response.zone[i]}</p><p>${response.neighborhood[i]}</p><p>${response.location[i]}</p><p>${response.occur_time[i]} ${response.occur_day[i]} ${response.occur_date[i]}</p>`)
+                )
+        }
+        else if(response.occur_day[i] == "Sunday"){
+            sundayMarkers.push(
+                L.circle([response.lat[i], response.long[i]], 
+                    {
+                        //stroke: false,
+                        fillOpacity: 0.75,
+                        color: chooseColorZone(response.crime_type[i]),
+                        fillColor: chooseColorZone(response.crime_type[i]),
+                        //radius: 100
+                    }).bindPopup(`<h3>Offense ID: ${response.offense_id[i]}</h3><hr><p>${response.crime_type[i]}</p><p>ZONE ${response.zone[i]}</p><p>${response.neighborhood[i]}</p><p>${response.location[i]}</p><p>${response.occur_time[i]} ${response.occur_day[i]} ${response.occur_date[i]}</p>`)
+                )
+        }
+
+        
+
 
       }
 
@@ -85,31 +272,75 @@ zoneMarkers = [];
 
       var typeLayer = L.layerGroup(typeMarkers);
       var zoneLayer = L.layerGroup(zoneMarkers);
+      var larcenyVLayer = L.layerGroup(larcenyVMarkers);
+      var larcenyNVLayer = L.layerGroup(larcenyNVMarkers);
+      var aggAssaultLayer = L.layerGroup(aggAssaultMarkers);
+      var robberyLayer = L.layerGroup(robberyMarkers);
+      var burglaryLayer = L.layerGroup(burglaryMarkers);
+      var autoTheftLayer = L.layerGroup(autoTheftMarkers);
+      var homicideLayer = L.layerGroup(homicideMarkers);
+      var mondayLayer = L.layerGroup(mondayMarkers);
+      var tuesdayLayer = L.layerGroup(tuesdayMarkers);
+      var wednesdayLayer = L.layerGroup(wednesdayMarkers);
+      var thursdayLayer = L.layerGroup(thursdayMarkers);
+      var fridayLayer = L.layerGroup(fridayMarkers);
+      var saturdayLayer = L.layerGroup(saturdayMarkers);
+      var sundayLayer = L.layerGroup(sundayMarkers);
 
       var baseMaps = {
         Street: street
       };
 
-      var overlayMaps = {
-        CrimeType: typeLayer,
-        Zones: zoneLayer
+      var overlayMaps1 = {
+        //CrimeType: typeLayer,
+        Zones: zoneLayer,
       };
 
-      var myMap = L.map("map", {
+      var overlayMaps2 = {
+        LarcenyFromVehicle: larcenyVLayer,
+        LarcenyNonVehicle: larcenyNVLayer,
+        AggAssault: aggAssaultLayer,
+        Robbery: robberyLayer,
+        Burglary: burglaryLayer,
+        AutoTheft: autoTheftLayer,
+        Homicide: homicideLayer
+      }
+
+      var overlayMaps3 = {
+          Monday: mondayLayer,
+          Tuesday: tuesdayLayer,
+          Wednesday: wednesdayLayer,
+          Thursday: thursdayLayer,
+          Friday: fridayLayer,
+          Saturday: saturdayLayer,
+          Sunday: sundayLayer
+      }
+
+    //   var myMap1 = L.map("map", {
+    //     center: [33.75, -84.4],
+    //     zoom: 11.45,
+    //     layers: [street,zoneLayer]
+    //   });
+
+    //   L.control.layers(baseMaps, overlayMaps1).addTo(myMap1);
+
+    //   var myMap2 = L.map("map", {
+    //     center: [33.75, -84.4],
+    //     zoom: 11.45,
+    //     layers: [street]
+    //   });
+
+    //   L.control.layers(baseMaps, overlayMaps2).addTo(myMap2);
+
+      var myMap3 = L.map("map", {
         center: [33.75, -84.4],
         zoom: 11.45,
-        layers: [street,zoneLayer]
+        layers: [street]
       });
 
-      L.control.layers(baseMaps, overlayMaps).addTo(myMap);
-
+      L.control.layers(baseMaps, overlayMaps3).addTo(myMap3);
 
         });
-
-
-
-
-
 
 
 
